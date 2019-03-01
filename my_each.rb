752129b885aee2b2daa745a20b50da37a5c63330
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-01 02:44:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if x = 0 
+    while x < array.length 
+    x = x + 1 
+ end 
+end
+
+
+ my_each(array)
+  my_each(collection) do |i|
+    puts i
+ end
 end
